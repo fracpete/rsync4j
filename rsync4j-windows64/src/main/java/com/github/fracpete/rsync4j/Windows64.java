6f350bd0330ac07eec76bc9eca6e8e709431e631
Peter Reutemann <fracpete@gmail.com>
2025-08-01 10:09:19 +1200

    added dummy class to get source/javadoc artifacts generated

---
+++ rsync4j-windows64/src/main/java/com/github/fracpete/rsync4j/Windows64.java
@@ -0,0 +1,30 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Windows64.java
+ * Copyright (C) 2025 University of Waikato, Hamilton, New Zealand
+ */
+
+package com.github.fracpete.rsync4j;
+
+/**
+ * Dummy class to get source/javadoc jar generated.
+ *
+ * @author fracpete (fracpete at waikato dot ac dot nz)
+ */
+public class Windows64 {
+
+}
